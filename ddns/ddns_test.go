966649d1d16684b05ae13ef4438702b0d5c2dbba
David Claridge <daave@daave.com>
2020-04-15 14:58:19 -0700

    add signal handling, rename packages

---
+++ ddns/ddns_test.go
@@ -0,0 +1,44 @@
+package ddns
+
+import (
+	"net/http/httptest"
+	"testing"
+)
+
+func TestUpdateValidation(t *testing.T) {
+	h := Router()
+	for name, tc := range map[string]struct {
+		url    string
+		status int
+	}{
+		"valid":                    {"https://ddns.c6e.me/update/c6e.me/hunter2/127.0.0.1", 200},
+		"valid-params":             {"https://ddns.c6e.me/update?domains=c6e.me&token=hunter2&ip=127.0.0.1", 200},
+		"valid-auto-ip":            {"https://ddns.c6e.me/update/c6e.me/hunter2", 200},
+		"valid-auto-ip-params":     {"https://ddns.c6e.me/update?domains=c6e.me&token=hunter2", 200},
+		"valid-multiple-domains":   {"https://ddns.c6e.me/update?domains=c6e.me,daave.com&token=hunter2", 200},
+		"valid-ipv6":               {"https://ddns.c6e.me/update?domains=c6e.me&token=hunter2&ip=::1", 200},
+		"valid-ipv4-and-v6":        {"https://ddns.c6e.me/update?domains=c6e.me&token=hunter2&ip=127.0.0.1&ipv6=::1", 200},
+		"valid-ipv6-param":         {"https://ddns.c6e.me/update?domains=c6e.me&token=hunter2&ipv6=::1", 200},
+		"invalid-ipv4":             {"https://ddns.c6e.me/update?domains=c6e.me&token=hunter2&ip=10.0.0.0.1", 400},
+		"invalid-ipv4-blatant":     {"https://ddns.c6e.me/update?domains=c6e.me&token=hunter2&ip=text", 400},
+		"invalid-ipv6":             {"https://ddns.c6e.me/update?domains=c6e.me&token=hunter2&ipv6=:::1", 400},
+		"invalid-ipv6-blatant":     {"https://ddns.c6e.me/update?domains=c6e.me&token=hunter2&ipv6=text", 400},
+		"invalid-domain":           {"https://ddns.c6e.me/update/c$e.me/hunter2", 400},
+		"invalid-multiple-domains": {"https://ddns.c6e.me/update?domains=c6e.me,d@@ve.com&token=hunter2", 400},
+		"domains-param-typo":       {"https://ddns.c6e.me/update?domain=c6e.me&token=hunter2", 400},
+		"missing-auth":             {"https://ddns.c6e.me/update?domains=c6e.me", 401},
+		"incorrect-auth":           {"https://ddns.c6e.me/update?domains=c6e.me&token=hunter7", 401},
+	} {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			req := httptest.NewRequest("GET", tc.url, nil)
+			w := httptest.NewRecorder()
+			h.ServeHTTP(w, req)
+			status := w.Result().StatusCode
+			if status != tc.status {
+				t.Errorf("unexpected status code, got %d, want %d", status, tc.status)
+			}
+		})
+
+	}
+}
--- server/server.go
+++ server/server.go
@@ -1,134 +1,44 @@
-package server
+package main
 
 import (
-	"fmt"
-	"io"
-	"net"
+	"context"
+	"log"
 	"net/http"
-	"strings"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
-	"ddns/netutil"
-
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
+	"ddns/ddns"
 )
 
-func Router() *mux.Router {
-	r := mux.NewRouter()
-	r.HandleFunc("/update", UpdateHandler)
-	r.HandleFunc("/update/{domain}/{token}", UpdateHandler)
-	r.HandleFunc("/update/{domain}/{token}/{ip}", UpdateHandler)
-	r.Use(handlers.ProxyHeaders)
-	return r
-}
+func main() {
+	srv := &http.Server{Addr: ":8080", Handler: ddns.Router()}
 
-func UpdateHandler(w http.ResponseWriter, r *http.Request) {
-	domain := r.FormValue("domains")
-	token := r.FormValue("token")
-	ip := r.FormValue("ip")
-	ipv6 := r.FormValue("ipv6")
-	verbose := r.FormValue("verbose") == "true"
-	// clear := r.FormValue("clear") == "true"
-	if domain == "" {
-		v := mux.Vars(r)
-		domain = v["domain"]
-		token = v["token"]
-		ip = v["ip"]
-	}
-	domains := strings.Split(domain, ",")
-
-	w.Header().Set("Content-Type", "text/plain")
-
-	if len(domains) == 0 {
-		w.WriteHeader(http.StatusBadRequest)
-		io.WriteString(w, "KO\n")
-		if verbose {
-			io.WriteString(w, "no domain(s) provided\n")
+	var wg sync.WaitGroup
+	go func() {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
+		sig := <-sigs
+		signal.Stop(sigs) // to allow force-exit on double ^C
+		wg.Add(1)
+		defer wg.Done()
+		log.Printf("received %s, shutting down", sig)
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("error whilst shutting down HTTP server: %v\n", err)
 		}
-		return
-	}
+		cancel()
+	}()
 
-	for _, domain := range domains {
-		if !netutil.IsDomainName(domain) {
-			w.WriteHeader(http.StatusBadRequest)
-			io.WriteString(w, "KO\n")
-			if verbose {
-				io.WriteString(w, fmt.Sprintf("invalid domain: %q\n", domain))
-			}
-			return
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Printf("error from ListenAndServe: %v\n", err)
 		}
-	}
+	}()
 
-	if token != "hunter2" { // TODO: auth
-		w.WriteHeader(http.StatusUnauthorized)
-		io.WriteString(w, "KO\n")
-		if verbose {
-			io.WriteString(w, "token should be 'hunter2' 🙈\n")
-		}
-		return
-	}
-
-	if ip == "" && ipv6 == "" {
-		// Try to auto-detect client IP
-		ip = r.RemoteAddr
-		if ip == "" {
-			w.WriteHeader(http.StatusInternalServerError)
-			io.WriteString(w, "KO\n")
-			if verbose {
-				io.WriteString(w, "unable to determine client IP address\n")
-			}
-			return
-		}
-		if host, _, err := net.SplitHostPort(ip); err == nil {
-			ip = host // strip port if present
-		}
-	}
-
-	if ipv6 != "" {
-		// Validate explicit IPv6 address
-		parsedIP := net.ParseIP(ipv6)
-		if parsedIP == nil {
-			w.WriteHeader(http.StatusBadRequest)
-			io.WriteString(w, "KO\n")
-			if verbose {
-				io.WriteString(w, fmt.Sprintf("invalid IP v6 address: %q\n", ipv6))
-			}
-			return
-		}
-
-		// TODO: removed this check since it breaks valid 4-to-6 addresses in the 0:0:0:0:0:ffff:* range
-		// if parsedIP.To4() != nil {
-		// 	w.WriteHeader(http.StatusBadRequest)
-		// 	io.WriteString(w, "KO\n")
-		// 	if verbose {
-		// 		io.WriteString(w, fmt.Sprintf("IPv4 address provided in IPv6 field: %q\n", ipv6))
-		// 	}
-		// 	return
-		// }
-	}
-
-	if ipv6 == "" {
-		// No explicit IPv6 addr, determine whether IP is v4 or v6.
-		parsedIP := net.ParseIP(ip)
-		if parsedIP == nil {
-			w.WriteHeader(http.StatusBadRequest)
-			io.WriteString(w, "KO\n")
-			if verbose {
-				io.WriteString(w, fmt.Sprintf("invalid IP address: %q\n", ip))
-			}
-			return
-		}
-		if parsedIP.To4() == nil {
-			ipv6 = ip
-			ip = ""
-		}
-	}
-
-	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, "OK\n")
-	if verbose {
-		io.WriteString(w, ip+"\n")
-		io.WriteString(w, ipv6+"\n")
-		io.WriteString(w, "UPDATED\n") // or NOCHANGE
-	}
+	wg.Wait() // wait for both ListenAndServe & Shutdown to return, as needed
 }
